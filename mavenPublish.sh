c58c38caf63334af4340d5a9a56d7d7c181caf0b
tarif <tarifchakdar@gmail.com>
2025-09-22 21:17:15 +0400

    Remove legacy maven publish scripts
    This commit removes the `mavenPublish.bat` and `mavenPublish.sh` scripts, as they are no longer needed. Publishing is now handled by the Gradle Publish Plugin and GitHub Actions workflows.
    
    Key changes:
    - Deleted `mavenPublish.bat`.
    - Deleted `mavenPublish.sh`.

--- mavenPublish.sh
+++
@@ -1,7 +0,0 @@
-# Local check
-./gradlew publishToMavenLocal
-
-# Maven Central
-./gradlew publishAllPublicationsToMavenCentralRepository
-
-./gradlew :network:publishAndroidDebugPublicationToMavenCentralRepository --no-configuration-cache
